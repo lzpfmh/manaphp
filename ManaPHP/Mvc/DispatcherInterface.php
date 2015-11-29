ea46a1b62a8025fee244cbeb23f4ea6dc2cdb388
ManaPHP Mark <manaphp@qq.com>
2015-11-29 17:11:40 +0800

    add DispatcherInterface.php

---
+++ ManaPHP/Mvc/DispatcherInterface.php
@@ -0,0 +1,60 @@
+<?php 
+
+namespace ManaPHP\Mvc {
+
+	/**
+	 * ManaPHP\Mvc\DispatcherInterface initializer
+	 */
+	
+	interface DispatcherInterface extends \ManaPHP\DispatcherInterface {
+
+		/**
+		 * Sets the default controller suffix
+		 *
+		 * @param string $controllerSuffix
+		 */
+		public function setControllerSuffix($controllerSuffix);
+
+
+		/**
+		 * Sets the default controller name
+		 *
+		 * @param string $controllerName
+		 */
+		public function setDefaultController($controllerName);
+
+
+		/**
+		 * Sets the controller name to be dispatched
+		 *
+		 * @param string $controllerName
+		 * @param bool $isExact If true, the name should not be mangled in any way
+		 */
+		public function setControllerName($controllerName, $isExact=null);
+
+
+		/**
+		 * Gets last dispatched controller name
+		 *
+		 * @return string
+		 */
+		public function getControllerName();
+
+
+		/**
+		 * Returns the latest dispatched controller
+		 *
+		 * @return \ManaPHP\Mvc\ControllerInterface
+		 */
+		public function getLastController();
+
+
+		/**
+		 * Returns the active controller in the dispatcher
+		 *
+		 * @return \ManaPHP\Mvc\ControllerInterface
+		 */
+		public function getActiveController();
+
+	}
+}
