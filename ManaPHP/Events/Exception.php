8496833ea0b53d98a4348a89842fed926bf26907
ManaPHP Mark <manaphp@qq.com>
2015-11-29 18:00:53 +0800

    add Events/Exception.php

---
+++ ManaPHP/Events/Exception.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace ManaPHP\Events {
+
+	/**
+	 * ManaPHP\Events\Exception
+	 *
+	 * Exceptions thrown in ManaPHP\Events will use this class
+	 *
+	 */
+	
+	class Exception extends \ManaPHP\Exception {
+	}
+}
