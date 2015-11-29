46a2bac4a9dcb856023ebb289446f10a3c3b1b5e
ManaPHP Mark <manaphp@qq.com>
2015-11-29 18:09:14 +0800

    implement Di/Service.php

--- ManaPHP/Di/Service.php
+++ ManaPHP/Di/Service.php
@@ -1,79 +1,59 @@
 <?php 
 
-namespace ManaPHP\DI {
+namespace ManaPHP\Di {
 
 	/**
-	 * ManaPHP\DI\Service
+	 * ManaPHP\Di\Service
 	 *
 	 * Represents individually a service in the services container
 	 *
 	 *<code>
-	 * $service = new ManaPHP\DI\Service('request', 'ManaPHP\Http\Request');
+	 * $service = new ManaPHP\Di\Service('request', 'ManaPHP\Http\Request');
 	 * $request = $service->resolve();
 	 *<code>
 	 *
 	 */
 	
-	class Service implements \ManaPHP\DI\ServiceInterface {
+	class Service implements \ManaPHP\Di\ServiceInterface {
 
 		/**
-		 * \ManaPHP\DI\Service
+		 * @var string
+		 */
+		protected $_name;
+
+		/**
+		 * @var callable|string|object
+		 */
+		protected $_definition;
+
+		/**
+		 * @var boolean
+		 */
+		protected $_shared;
+
+		/**
+		 * @var boolean
+		 */
+		protected $_resolved=false;
+
+		/**
+		 * @var object
+		 */
+		protected $_sharedInstance;
+
+
+		/**
+		 * \ManaPHP\Di\Service
 		 *
 		 * @param string $name
-		 * @param mixed $definition
+		 * @param string|callable|object $definition
 		 * @param boolean $shared
 		 */
-		public function __construct($name, $definition, $shared=null){ }
-
-
-		/**
-		 * Returns the service's name
-		 *
-		 * @param string
-		 * @return string
-		 */
-		public function getName(){ }
-
-
-		/**
-		 * Sets if the service is shared or not
-		 *
-		 * @param boolean $shared
-		 */
-		public function setShared($shared){ }
-
-
-		/**
-		 * Check whether the service is shared or not
-		 *
-		 * @return boolean
-		 */
-		public function isShared(){ }
-
-
-		/**
-		 * Sets/Resets the shared instance related to the service
-		 *
-		 * @param mixed $sharedInstance
-		 */
-		public function setSharedInstance($sharedInstance){ }
-
-
-		/**
-		 * Set the service definition
-		 *
-		 * @param mixed $definition
-		 */
-		public function setDefinition($definition){ }
-
-
-		/**
-		 * Returns the service definition
-		 *
-		 * @return mixed
-		 */
-		public function getDefinition(){ }
-
+		public function __construct($name, $definition, $shared){
+			$this->_name =$name;
+			$this->_definition =$definition;
+			$this->_shared =$shared;
+		}
 
 		/**
 		 * Resolves the service
@@ -81,44 +61,46 @@ namespace ManaPHP\DI {
 		 * @param array $parameters
 		 * @param \ManaPHP\DiInterface $dependencyInjector
 		 * @return object
+		 * @throws \ManaPHP\Exception
 		 */
-		public function resolve($parameters=null, $dependencyInjector=null){ }
+		public function resolve($parameters=null, $dependencyInjector=null){
+            if($this->_shared &&$this->_sharedInstance !==null){
+				return $this->_sharedInstance;
+            }
 
+			$instance =null;
+            $definition =$this->_definition;
+			$canNotBeResolvedError='Service \''. $this->_name . '\' cannot be resolved';
 
-		/**
-		 * Changes a parameter in the definition without resolve the service
-		 *
-		 * @param int $position
-		 * @param array $parameter
-		 * @return \ManaPHP\DI\Service
-		 */
-		public function setParameter($position, $parameter){ }
+            if(is_string($definition)){
+                if(class_exists($definition)){
+                    if(is_array($parameters)){
+                        $instance =new $definition($parameters);
+                    }else{
+                        $instance =new $definition();
+                    }
+                }else{
+					throw new Exception($canNotBeResolvedError);
+                }
+            }elseif($definition instanceof \Closure){
+				if(is_array($parameters)){
+					$instance =call_user_func_array($definition,$parameters);
+				}else{
+					$instance =call_user_func($definition);
+				}
+			}elseif(is_object($definition)){
+				$instance =$definition;
+			}else{
+				throw new Exception($canNotBeResolvedError);
+			}
 
+			if($this->_shared){
+				$this->_sharedInstance =$instance;
+			}
 
-		/**
-		 * Returns a parameter in a specific position
-		 *
-		 * @param int $position
-		 * @return array
-		 */
-		public function getParameter($position){ }
+			$this->_resolved =true;
 
-
-		/**
-		 * Returns true if the service was resolved
-		 *
-		 * @return bool
-		 */
-		public function isResolved(){ }
-
-
-		/**
-		 * Restore the internal state of a service
-		 *
-		 * @param array $attributes
-		 * @return \ManaPHP\DI\Service
-		 */
-		public static function __set_state($properties=null){ }
-
+			return $instance;
+        }
 	}
 }
