50a6cb112a606172dee4d73e126965f714adc2dc
ManaPHP Mark <manaphp@qq.com>
2015-11-29 18:15:27 +0800

    add Di/Exception.php

---
+++ ManaPHP/Di/Exception.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace ManaPHP\DI {
+
+	/**
+	 * ManaPHP\DI\Exception
+	 *
+	 * Exceptions thrown in ManaPHP\DI will use this class
+	 *
+	 */
+	
+	class Exception extends \ManaPHP\Exception {
+	}
+}
