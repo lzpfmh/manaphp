9e3d09fe08c15f9e1dcc86569dc527d71a8421c6
ManaPHP Mark <manaphp@qq.com>
2015-11-29 18:02:14 +0800

    add Events/EventsAwareInterface.php

---
+++ ManaPHP/Events/EventsAwareInterface.php
@@ -0,0 +1,27 @@
+<?php 
+
+namespace ManaPHP\Events {
+
+	/**
+	 * ManaPHP\Events\EventsAwareInterface initializer
+	 */
+	
+	interface EventsAwareInterface {
+
+		/**
+		 * Sets the events manager
+		 *
+		 * @param \ManaPHP\Events\ManagerInterface $eventsManager
+		 */
+		public function setEventsManager($eventsManager);
+
+
+		/**
+		 * Returns the internal event manager
+		 *
+		 * @return \ManaPHP\Events\ManagerInterface
+		 */
+		public function getEventsManager();
+
+	}
+}
