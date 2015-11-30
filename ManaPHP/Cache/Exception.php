5fdf3d18450156b87330fde0d5e06cd2175294ce
ManaPHP Mark <manaphp@qq.com>
2015-11-30 20:30:28 +0800

    Add        ManaPHP/Cache/Exception.php

---
+++ ManaPHP/Cache/Exception.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace ManaPHP\Cache {
+
+	/**
+	 * ManaPHP\Cache\Exception
+	 *
+	 * Exceptions thrown in ManaPHP\Cache will use this class
+	 *
+	 */
+	
+	class Exception extends \ManaPHP\Exception {
+	}
+}
