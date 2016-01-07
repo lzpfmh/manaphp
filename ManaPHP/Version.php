951da624e7f1801f518d77532f831c32329d7335
ManaPHP Mark <manaphp@qq.com>
2016-01-07 23:41:31 +0800

    Modified   ManaPHP/Version.php

--- ManaPHP/Version.php
+++ ManaPHP/Version.php
@@ -1,26 +1,27 @@
-<?php 
+<?php
 
 namespace ManaPHP {
 
-	/**
-	 * ManaPHP\Version
-	 *
-	 * This class allows to get the installed version of the framework
-	 */
-	
-	class Version {
+    /**
+     * ManaPHP\Version
+     *
+     * This class allows to get the installed version of the framework
+     */
+    class Version
+    {
 
-		/**
-		 * Returns the active version (string)
-		 *
-		 * <code>
-		 * echo \ManaPHP\Version::get();
-		 * </code>
-		 *
-		 * @return string
-		 */
-		public static function get(){
-			return '0.1.0';
-		}
-	}
+        /**
+         * Returns the active version (string)
+         *
+         * <code>
+         * echo \ManaPHP\Version::get();
+         * </code>
+         *
+         * @return string
+         */
+        public static function get()
+        {
+            return '0.1.0';
+        }
+    }
 }
