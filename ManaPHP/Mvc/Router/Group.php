f4d45d29c7ece41af28752cb3f54b9fa015ab909
ManaPHP Mark <manaphp@qq.com>
2016-01-07 23:43:12 +0800

    Modified   ManaPHP/Mvc/Router/Group.php

--- ManaPHP/Mvc/Router/Group.php
+++ ManaPHP/Mvc/Router/Group.php
@@ -1,236 +1,249 @@
-<?php 
+<?php
 
 namespace ManaPHP\Mvc\Router {
 
-	/**
-	 * ManaPHP\Mvc\Router\Group
-	 *
-	 * Helper class to create a group of routes with common attributes
-	 *
-	 * NOTE_PHP:
-	 * 		1.Hostname Constraints has been removed by PHP implementation
-	 * 		2. remove clear method
-	 *
-	 *<code>
-	 * $router = new ManaPHP\Mvc\Router();
-	 *
-	 * //Create a group with a common module and controller
-	 * $blog = new ManaPHP\Mvc\Router\Group(array(
-	 * 	'module' => 'blog',
-	 * 	'controller' => 'index'
-	 * ));
-	 *
-	 * //All the routes start with /blog
-	 * $blog->setPrefix('/blog');
-	 *
-	 * //Add a route to the group
-	 * $blog->add('/save', array(
-	 * 	'action' => 'save'
-	 * ));
-	 *
-	 * //Add another route to the group
-	 * $blog->add('/edit/{id}', array(
-	 * 	'action' => 'edit'
-	 * ));
-	 *
-	 * //This route maps to a controller different than the default
-	 * $blog->add('/blog', array(
-	 * 	'controller' => 'about',
-	 * 	'action' => 'index'
-	 * ));
-	 *
-	 * //Add the group to the router
-	 * $router->mount($blog);
-	 *</code>
-	 *
-	 */
-	
-	class Group implements GroupInterface {
+    /**
+     * ManaPHP\Mvc\Router\Group
+     *
+     * Helper class to create a group of routes with common attributes
+     *
+     * NOTE_PHP:
+     *        1.Hostname Constraints has been removed by PHP implementation
+     *        2. remove clear method
+     *
+     *<code>
+     * $router = new ManaPHP\Mvc\Router();
+     *
+     * //Create a group with a common module and controller
+     * $blog = new ManaPHP\Mvc\Router\Group(array(
+     *    'module' => 'blog',
+     *    'controller' => 'index'
+     * ));
+     *
+     * //All the routes start with /blog
+     * $blog->setPrefix('/blog');
+     *
+     * //Add a route to the group
+     * $blog->add('/save', array(
+     *    'action' => 'save'
+     * ));
+     *
+     * //Add another route to the group
+     * $blog->add('/edit/{id}', array(
+     *    'action' => 'edit'
+     * ));
+     *
+     * //This route maps to a controller different than the default
+     * $blog->add('/blog', array(
+     *    'controller' => 'about',
+     *    'action' => 'index'
+     * ));
+     *
+     * //Add the group to the router
+     * $router->mount($blog);
+     *</code>
+     *
+     */
+    class Group implements GroupInterface
+    {
 
-		/*
-		 * var string
-		 */
-		protected $_prefix;
+        /*
+         * var string
+         */
+        protected $_prefix;
 
-		/**
-		 * @var array
-		 */
-		protected $_paths;
+        /**
+         * @var array
+         */
+        protected $_paths;
 
-		/**
-		 * @var \ManaPHP\Mvc\Router\RouteInterface[]
-		 */
-		protected $_routes;
-
-
-		/**
-		 * \ManaPHP\Mvc\Router\Group constructor
-		 *
-		 * @param array $paths
-		 */
-		public function __construct($paths=null){
-			if(is_array($paths)){
-				$this->_paths =$paths;
-			}
-		}
+        /**
+         * @var \ManaPHP\Mvc\Router\RouteInterface[]
+         */
+        protected $_routes;
 
 
-		/**
-		 * Set a common uri prefix for all the routes in this group
-		 *
-		 * @param string $prefix
-		 * @return static
-		 */
-		public function setPrefix($prefix){
-			$this->_prefix=$prefix;
-			return $this;
-		}
+        /**
+         * \ManaPHP\Mvc\Router\Group constructor
+         *
+         * @param array $paths
+         */
+        public function __construct($paths = null)
+        {
+            if (is_array($paths)) {
+                $this->_paths = $paths;
+            }
+        }
 
 
-		/**
-		 * Set common paths for all the routes in the group
-		 *
-		 * @param array $paths
-		 * @return static
-		 */
-		public function setPaths($paths){
-			$this->_paths =$paths;
-			return $this;
-		}
+        /**
+         * Set a common uri prefix for all the routes in this group
+         *
+         * @param string $prefix
+         * @return static
+         */
+        public function setPrefix($prefix)
+        {
+            $this->_prefix = $prefix;
+            return $this;
+        }
 
 
-		/**
-		 * Returns the routes added to the group
-		 *
-		 * @return \ManaPHP\Mvc\Router\RouteInterface[]
-		 */
-		public function getRoutes(){
-			return $this->_routes;
-		}
+        /**
+         * Set common paths for all the routes in the group
+         *
+         * @param array $paths
+         * @return static
+         */
+        public function setPaths($paths)
+        {
+            $this->_paths = $paths;
+            return $this;
+        }
 
 
-		/**
-		 * Adds a route applying the common attributes
-		 *
-		 * @param string $pattern
-		 * @param array $paths
-		 * @param array $httpMethods
-		 * @return \ManaPHP\Mvc\Router\RouteInterface
-		 * @throws \ManaPHP\Mvc\Router\Exception
-		 */
-		protected function _addRoute($pattern, $paths=null, $httpMethods=null){
-			$route =new Route($this->_prefix.$pattern,is_array($paths)?array_merge($this->_paths,$paths):$this->_paths, $httpMethods);
-			$this->_routes[]=$route;
-			return $route;
-		}
+        /**
+         * Returns the routes added to the group
+         *
+         * @return \ManaPHP\Mvc\Router\RouteInterface[]
+         */
+        public function getRoutes()
+        {
+            return $this->_routes;
+        }
 
 
-		/**
-		 * Adds a route to the router on any HTTP method
-		 *
-		 *<code>
-		 * $router->add('/about', 'About::index');
-		 *</code>
-		 *
-		 * @param string $pattern
-		 * @param array $paths
-		 * @param string $httpMethods
-		 * @return \ManaPHP\Mvc\Router\RouteInterface
-		 * @throws \ManaPHP\Mvc\Router\Exception
-		 */
-		public function add($pattern, $paths=null, $httpMethods=null){
-			return $this->_addRoute($pattern,$paths,$httpMethods);
-		}
+        /**
+         * Adds a route applying the common attributes
+         *
+         * @param string $pattern
+         * @param array $paths
+         * @param array $httpMethods
+         * @return \ManaPHP\Mvc\Router\RouteInterface
+         * @throws \ManaPHP\Mvc\Router\Exception
+         */
+        protected function _addRoute($pattern, $paths = null, $httpMethods = null)
+        {
+            $route = new Route($this->_prefix . $pattern, is_array($paths) ? array_merge($this->_paths, $paths) : $this->_paths, $httpMethods);
+            $this->_routes[] = $route;
+            return $route;
+        }
 
 
-		/**
-		 * Adds a route to the router that only match if the HTTP method is GET
-		 *
-		 * @param string $pattern
-		 * @param array $paths
-		 * @return \ManaPHP\Mvc\Router\RouteInterface
-		 * @throws \ManaPHP\Mvc\Router\Exception
-		 */
-		public function addGet($pattern, $paths=null){
-			return $this->_addRoute($pattern, $paths, 'GET');
-		}
+        /**
+         * Adds a route to the router on any HTTP method
+         *
+         *<code>
+         * $router->add('/about', 'About::index');
+         *</code>
+         *
+         * @param string $pattern
+         * @param array $paths
+         * @param string $httpMethods
+         * @return \ManaPHP\Mvc\Router\RouteInterface
+         * @throws \ManaPHP\Mvc\Router\Exception
+         */
+        public function add($pattern, $paths = null, $httpMethods = null)
+        {
+            return $this->_addRoute($pattern, $paths, $httpMethods);
+        }
 
 
-		/**
-		 * Adds a route to the router that only match if the HTTP method is POST
-		 *
-		 * @param string $pattern
-		 * @param array $paths
-		 * @return \ManaPHP\Mvc\Router\RouteInterface
-		 * @throws \ManaPHP\Mvc\Router\Exception
-		 */
-		public function addPost($pattern, $paths=null){
-			return $this->_addRoute($pattern, $paths, 'POST');
-		}
+        /**
+         * Adds a route to the router that only match if the HTTP method is GET
+         *
+         * @param string $pattern
+         * @param array $paths
+         * @return \ManaPHP\Mvc\Router\RouteInterface
+         * @throws \ManaPHP\Mvc\Router\Exception
+         */
+        public function addGet($pattern, $paths = null)
+        {
+            return $this->_addRoute($pattern, $paths, 'GET');
+        }
 
 
-		/**
-		 * Adds a route to the router that only match if the HTTP method is PUT
-		 *
-		 * @param string $pattern
-		 * @param array $paths
-		 * @return \ManaPHP\Mvc\Router\RouteInterface
-		 * @throws \ManaPHP\Mvc\Router\Exception
-		 */
-		public function addPut($pattern, $paths=null){
-			return $this->_addRoute($pattern, $paths, 'PUT');
-		}
+        /**
+         * Adds a route to the router that only match if the HTTP method is POST
+         *
+         * @param string $pattern
+         * @param array $paths
+         * @return \ManaPHP\Mvc\Router\RouteInterface
+         * @throws \ManaPHP\Mvc\Router\Exception
+         */
+        public function addPost($pattern, $paths = null)
+        {
+            return $this->_addRoute($pattern, $paths, 'POST');
+        }
 
 
-		/**
-		 * Adds a route to the router that only match if the HTTP method is PATCH
-		 *
-		 * @param string $pattern
-		 * @param array $paths
-		 * @return \ManaPHP\Mvc\Router\RouteInterface
-		 * @throws \ManaPHP\Mvc\Router\Exception
-		 */
-		public function addPatch($pattern, $paths=null){
-			return $this->_addRoute($pattern, $paths, 'PATCH');
-		}
+        /**
+         * Adds a route to the router that only match if the HTTP method is PUT
+         *
+         * @param string $pattern
+         * @param array $paths
+         * @return \ManaPHP\Mvc\Router\RouteInterface
+         * @throws \ManaPHP\Mvc\Router\Exception
+         */
+        public function addPut($pattern, $paths = null)
+        {
+            return $this->_addRoute($pattern, $paths, 'PUT');
+        }
 
 
-		/**
-		 * Adds a route to the router that only match if the HTTP method is DELETE
-		 *
-		 * @param string $pattern
-		 * @param array $paths
-		 * @return \ManaPHP\Mvc\Router\RouteInterface
-		 * @throws \ManaPHP\Mvc\Router\Exception
-		 */
-		public function addDelete($pattern, $paths=null){
-			return $this->_addRoute($pattern, $paths, 'DELETE');
-		}
+        /**
+         * Adds a route to the router that only match if the HTTP method is PATCH
+         *
+         * @param string $pattern
+         * @param array $paths
+         * @return \ManaPHP\Mvc\Router\RouteInterface
+         * @throws \ManaPHP\Mvc\Router\Exception
+         */
+        public function addPatch($pattern, $paths = null)
+        {
+            return $this->_addRoute($pattern, $paths, 'PATCH');
+        }
 
 
-		/**
-		 * Add a route to the router that only match if the HTTP method is OPTIONS
-		 *
-		 * @param string $pattern
-		 * @param array $paths
-		 * @return \ManaPHP\Mvc\Router\RouteInterface
-		 * @throws \ManaPHP\Mvc\Router\Exception
-		 */
-		public function addOptions($pattern, $paths=null){
-			return $this->_addRoute($pattern, $paths, 'OPTIONS');
-		}
+        /**
+         * Adds a route to the router that only match if the HTTP method is DELETE
+         *
+         * @param string $pattern
+         * @param array $paths
+         * @return \ManaPHP\Mvc\Router\RouteInterface
+         * @throws \ManaPHP\Mvc\Router\Exception
+         */
+        public function addDelete($pattern, $paths = null)
+        {
+            return $this->_addRoute($pattern, $paths, 'DELETE');
+        }
 
 
-		/**
-		 * Adds a route to the router that only match if the HTTP method is HEAD
-		 *
-		 * @param string $pattern
-		 * @param array $paths
-		 * @return \ManaPHP\Mvc\Router\RouteInterface
-		 * @throws \ManaPHP\Mvc\Router\Exception
-		 */
-		public function addHead($pattern, $paths=null){
-			return $this->_addRoute($pattern, $paths, 'HEAD');
-		}
-	}
+        /**
+         * Add a route to the router that only match if the HTTP method is OPTIONS
+         *
+         * @param string $pattern
+         * @param array $paths
+         * @return \ManaPHP\Mvc\Router\RouteInterface
+         * @throws \ManaPHP\Mvc\Router\Exception
+         */
+        public function addOptions($pattern, $paths = null)
+        {
+            return $this->_addRoute($pattern, $paths, 'OPTIONS');
+        }
+
+
+        /**
+         * Adds a route to the router that only match if the HTTP method is HEAD
+         *
+         * @param string $pattern
+         * @param array $paths
+         * @return \ManaPHP\Mvc\Router\RouteInterface
+         * @throws \ManaPHP\Mvc\Router\Exception
+         */
+        public function addHead($pattern, $paths = null)
+        {
+            return $this->_addRoute($pattern, $paths, 'HEAD');
+        }
+    }
 }
