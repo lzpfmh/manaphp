ca6f268a50cc21864ad6347b092be361e823a3db
ManaPHP Mark <manaphp@qq.com>
2016-01-07 23:42:09 +0800

    Modified   ManaPHP/Mvc/Router/RouteInterface.php

--- ManaPHP/Mvc/Router/RouteInterface.php
+++ ManaPHP/Mvc/Router/RouteInterface.php
@@ -1,36 +1,36 @@
-<?php 
+<?php
 
 namespace ManaPHP\Mvc\Router {
 
-	/**
-	 * ManaPHP\Mvc\Router\RouteInterface initializer
-	 */
-	
-	interface RouteInterface {
+    /**
+     * ManaPHP\Mvc\Router\RouteInterface initializer
+     */
+    interface RouteInterface
+    {
 
-		/**
-		 * Sets a callback that is called if the route is matched.
-		 * The developer can implement any arbitrary conditions here
-		 * If the callback returns false the route is treated as not matched
-		 *
-		 * @param callable callback
-		 * @return static
-		 */
-		public function beforeMatch($callback);
+        /**
+         * Sets a callback that is called if the route is matched.
+         * The developer can implement any arbitrary conditions here
+         * If the callback returns false the route is treated as not matched
+         *
+         * @param callable callback
+         * @return static
+         */
+        public function beforeMatch($callback);
 
 
-		/**
-		 * Returns the paths
-		 *
-		 * @return array
-		 */
-		public function getPaths();
+        /**
+         * Returns the paths
+         *
+         * @return array
+         */
+        public function getPaths();
 
-		/**
-		 * @param string $handle_uri
-		 * @param array $matches
-		 * @return bool
-		 */
-		public function isMatched($handle_uri, &$matches);
-	}
+        /**
+         * @param string $handle_uri
+         * @param array $matches
+         * @return bool
+         */
+        public function isMatched($handle_uri, &$matches);
+    }
 }
