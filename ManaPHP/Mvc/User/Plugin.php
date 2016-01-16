4a3204cec35e4edaf488a4c55c0658c231d690f1
ManaPHP Mark <manaphp@qq.com>
2016-01-16 08:30:51 +0800

    Added      ManaPHP/Mvc/User/Plugin.php

---
+++ ManaPHP/Mvc/User/Plugin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mark
+ * Date: 2016/1/15
+ */
+namespace ManaPHP\Mvc\User;
+
+use ManaPHP\Di\Injectable;
+
+/**
+ * ManaPHP\Mvc\User\Plugin
+ *
+ * This class can be used to provide user plugins an easy access to services
+ * in the application
+ */
+class Plugin extends Injectable{
+
+}
