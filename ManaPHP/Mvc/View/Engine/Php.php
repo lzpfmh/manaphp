fed67fd0d7b0af406d01ac9afd4d0ee7a765384a
ManaPHP Mark <manaphp@qq.com>
2016-01-20 22:01:32 +0800

    Added      ManaPHP/Mvc/View/Engine/Php.php Added      ManaPHP/Mvc/View/EngineInterface.php

---
+++ ManaPHP/Mvc/View/Engine/Php.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace ManaPHP\Mvc\View\Engine {
+
+    use ManaPHP\Component;
+    use ManaPHP\Mvc\View\EngineInterface;
+
+    /**
+     * ManaPHP\Mvc\View\Engine\Php
+     *
+     * Adapter to use PHP itself as template engine
+     */
+
+    class Php extends Component implements EngineInterface{
+        protected $_view;
+
+        /**
+         * Php constructor.
+         * @param \ManaPHP\Mvc\ViewInterface $view
+         * @param \ManaPHP\DiInterface $dependencyInjector
+         */
+        public function __construct($view, $dependencyInjector = null)
+	    {
+		    $this->_view = $view;
+		    $this->_dependencyInjector = $dependencyInjector;
+	    }
+
+        /**
+         * Renders a view using the template engine
+         *
+         * @param string $file
+         * @param array $vars
+         * @param bool $mustClean
+         */
+        public function render($file, $vars=null, $mustClean=false){
+            if($mustClean){
+                ob_clean();
+            }
+
+            if(is_array($vars)){
+                extract($vars);
+            }
+
+            /** @noinspection PhpIncludeInspection */
+            require ($file);
+
+            if($mustClean){
+                $this->_view->setContent(ob_get_contents());
+            }
+        }
+    }
+}
---
+++ ManaPHP/Mvc/View/EngineInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ManaPHP\Mvc\View {
+
+    /**
+     * ManaPHP\Mvc\View\EngineInterface initializer
+     */
+
+    interface EngineInterface {
+
+        /**
+         * Php constructor.
+         * @param \ManaPHP\Mvc\ViewInterface $view
+         * @param \ManaPHP\DiInterface $dependencyInjector
+         */
+        public function __construct($view, $dependencyInjector = null);
+
+        /**
+         * Renders a view using the template engine
+         *
+         * @param string $file
+         * @param array $vars
+         * @param bool $mustClean
+         */
+        public function render($file, $vars=null, $mustClean=false);
+    }
+}
