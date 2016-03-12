b9cb7ff224d2dcde5037530d4e95b27ba9f8bd04
ManaPHP Mark <manaphp@qq.com>
2016-03-12 12:04:35 +0800

    Modified   Application/Config/Routes.php

--- Application/Config/Routes.php
+++ Application/Config/Routes.php
@@ -5,8 +5,7 @@
  * Date: 2015/11/22
  * Time: 12:31
  */
-$router =new \ManaPHP\Mvc\Router(false);
-$router->add('/',['controller'=>'index','action'=>'index']);
-$router->add('/test2',['controller'=>'index','action'=>'test2']);
+$router =new \ManaPHP\Mvc\Router();
+
 $router->removeExtraSlashes(true);
 return $router;
