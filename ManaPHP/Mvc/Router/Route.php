39b3b99075d315a6154674ecb9f908c43338e0f1
ManaPHP Mark <manaphp@qq.com>
2016-03-19 14:51:47 +0800

    Modified   ManaPHP/Mvc/Router/Route.php

--- ManaPHP/Mvc/Router/Route.php
+++ ManaPHP/Mvc/Router/Route.php
@@ -221,15 +221,19 @@ namespace ManaPHP\Mvc\Router {
                 $r = preg_match($this->_compiledPattern, $handle_uri, $matches);
                 if ($r === false) {
                     throw new Exception('--invalid PCRE: ' . $this->_compiledPattern . ' for ' . $this->_pattern);
+                }else if($r===1){
+                    return true;
+                }else{
+                    return false;
                 }
-
-                $is_matched = $r === 1;
             } else {
-                $is_matched = $this->_compiledPattern === $handle_uri;
-                $matches=[];
+                if($this->_compiledPattern === $handle_uri){
+                    $matches=[];
+                    return true;
+                }else{
+                    return false;
+                }
             }
-
-            return $is_matched;
         }
     }
 }
